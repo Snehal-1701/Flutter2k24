5f77589ea00f55a43590e82a1d56b4f3227e8c4f
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-07 23:22:57 +0530

    datatype

---
+++ ClassCode/DataType/code3.dart
@@ -0,0 +1,17 @@
+void main() {
+  int x = 10;
+  double y = 20.5;
+
+  print(x);
+  print(y);
+
+  print(x.runtimeType);
+  print(y.runtimeType);
+
+  // x = 50.7;
+  y = 30;
+
+  print(x);
+  print(y);
+
+}
