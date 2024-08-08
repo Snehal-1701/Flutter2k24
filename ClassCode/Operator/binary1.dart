987f7f978375a437f21ae6fdd081591213bdc9fd
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-08 22:32:58 +0530

    binary - arithmetic operator

---
+++ ClassCode/Operator/binary1.dart
@@ -0,0 +1,13 @@
+// Arithmetic operator
+
+void main() {
+  int x = 10;
+  int y = 4;
+
+  print(x+y);     //add
+  print(x-y);     //subtract
+  print(x*y);     //multiply
+  print(x/y);     //decimal division
+  print(x%y);     //modulus - remainder
+  print(x~/y);    //integer division
+}
