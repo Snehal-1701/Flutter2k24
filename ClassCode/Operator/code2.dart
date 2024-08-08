c5eb8e2e7b9c9af6d223dc95724d66a4a0940d28
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-08 23:09:28 +0530

    operator

---
+++ ClassCode/Operator/code2.dart
@@ -0,0 +1,16 @@
+void main() {
+  int x = 5;
+  int y = 6;
+
+  var ans = (++x < --y) || (x++ > ++y);
+
+  print(ans);
+  print(x);
+  print(y);
+
+  ans = (++x < --y) && (x++ > ++y);
+
+  print(ans);
+  print(x);
+  print(y);
+}
