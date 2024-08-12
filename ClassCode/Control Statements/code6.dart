07466148e6ba5355577c8bfe9440c1e2b6604e42
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-12 23:25:32 +0530

    Switch statement

---
+++ ClassCode/Control Statements/code6.dart
@@ -0,0 +1,17 @@
+// Switch case
+
+void main() {
+  double x = 1.5;
+  switch(x) {
+    case 1.5:
+      print("Monday");
+    case 2.5:
+      print("Tuesday");
+    case 3.5:
+      print("Wednesday");
+    case 4.5:
+      print("Thursday");
+    default:
+      print("Wrong Input");
+  }
+}
