53d5bd678f9f3d925bad923a4cbf44549f66db31
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-15 23:42:15 +0530

    Default parameter

---
+++ Dart/ClassCode/Functions/Parameter type/code4.dart
@@ -0,0 +1,15 @@
+// Parameter
+// 1) Positional
+// 2) Default
+// 3) Named
+
+// Default
+
+void empInfo(int empId, String empName, [String? cmpName]) {
+  print(empId);
+  print(empName);
+  print(cmpName);
+}
+void main() {
+  empInfo(12, "Snehal");
+}
