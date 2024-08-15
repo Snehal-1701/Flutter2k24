d820360061b6b8c5c8174286546ddfe25772e581
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-15 23:34:58 +0530

    Positional parameter

---
+++ Dart/ClassCode/Functions/Parameter type/code1.dart
@@ -0,0 +1,17 @@
+// Parameter
+// 1) Positional
+// 2) Default
+// 3) Named
+
+// positional parameters :
+// sequence and count of arguments matter
+// compulsory parameters
+
+void empInfo(int empId, String empName, String cmpName) {
+  print(empId);
+  print(empName);
+  print(cmpName);
+}
+void main() {
+  empInfo(12, "Snehal", "Core2web");
+}
