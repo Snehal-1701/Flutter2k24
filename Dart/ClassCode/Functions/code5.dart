f82bd2de8329ffff0092a8bef59dad485b3ab21e
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-15 23:25:26 +0530

    Functions with parameters and return value

--- Dart/ClassCode/Functions/code5.dart
+++ Dart/ClassCode/Functions/code5.dart
@@ -2,3 +2,13 @@
 
 import 'dart:io';
 
+void main() {
+  int num1 = 10;
+  int num2 = 20;
+  int retVal = add(num1, num2);
+  print("Add : $retVal");
+}
+
+int add(int num1, int num2) {
+  return num1+num2;
+}
