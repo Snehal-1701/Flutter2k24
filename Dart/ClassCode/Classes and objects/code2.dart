36123373d5a23c9c9bd72bbca19f669b58967095
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-16 16:13:55 +0530

    Classes and objects

---
+++ Dart/ClassCode/Classes and objects/code2.dart
@@ -0,0 +1,18 @@
+// Classes and objects
+
+void main() {
+  // properties
+  int x = 10;
+  String str = "Snehal";
+  double height = 5.2;
+
+  // methods
+  void info() {
+    
+  }
+
+  print(x.runtimeType);
+  print(str.runtimeType);
+  print(height.runtimeType);
+  print(info.runtimeType);
+}
