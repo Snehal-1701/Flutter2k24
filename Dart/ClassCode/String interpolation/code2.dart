44a5da5fcc5c416f5501eb6f662bad9ec390dfbc
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-16 16:34:12 +0530

    String interpolation

---
+++ Dart/ClassCode/String interpolation/code2.dart
@@ -0,0 +1,10 @@
+void main() {
+  int empId = 20;
+  String firstName = "Incubators";
+  String lastName = "Systems";
+
+  print("My employee id : $empId");
+  print("First name : $firstName, Last name : $lastName");
+
+  print("My firstName is $firstName and length is : ${firstName.length}");
+}
