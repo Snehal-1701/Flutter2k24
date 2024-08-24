c124c803adf02c7071086fe71a42e0ec6096bb8c
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-24 22:36:16 +0530

    Single level inheritence

---
+++ Dart/ClassCode/Inheritence/code1.dart
@@ -0,0 +1,18 @@
+class MacDIndia {
+  double burger = 150.0;
+  double fries = 90.0;
+
+  void food() {
+    print("Burger, price : $burger");
+    print("Fries, price : $fries");
+  }
+}
+
+class SinhgadMacD extends MacDIndia {
+
+}
+
+void main() {
+  SinhgadMacD obj = SinhgadMacD();
+  obj.food();
+}
