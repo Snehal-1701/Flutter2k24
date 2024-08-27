54018183e2e0c8b23429107e4844baa558f13295
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-27 22:26:51 +0530

    overriding method

---
+++ Dart/ClassCode/Polymorphism/code4.dart
@@ -0,0 +1,25 @@
+// overloading is not supported in dart
+
+class MacDIndia {
+  void food() {
+    print("Defined reciepe");
+  }
+
+  void marketing() {
+    print("Marketing");
+    // return 0;
+  }
+}
+
+class SinhgadMacD extends MacDIndia {
+  int marketing() {
+    print("Puneri marketing");
+    return 0;
+  }
+}
+
+void main() {
+  SinhgadMacD obj = SinhgadMacD();
+  obj.food();
+  obj.marketing();
+}
