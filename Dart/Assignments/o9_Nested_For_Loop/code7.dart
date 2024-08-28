b29b626235165b1a90144302b4d6702d118eaf48
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-28 17:02:38 +0530

    Pattern 7 - Nested For loop

---
+++ Dart/Assignments/o9_Nested_For_Loop/code7.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+    stdout.write("Enter no. of rows : ");
+    int row = int.parse(stdin.readLineSync()!);
+   
+    int num = 1;
+   
+    for(int i=1; i<=row; i++) {
+       
+        int num1 = num;
+       
+        for(int j=1; j<=i; j++) {
+           
+            stdout.write("$num1 ");
+            num1++;
+           
+        }
+       
+        num++;
+        print("");
+       
+    }
+}
