5c2160f011157ef69d3e5e04710397de067772b9
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-09-12 23:27:07 +0530

    List collection

---
+++ Dart/ClassCode/Collection/List/code1.dart
@@ -0,0 +1,10 @@
+void main() {
+  List player = ["Virat", "MSD", "Rohit", "KLRahul"];
+  print(player);
+
+  List player2 = ["Virat", "MSD", "Rohit", "KLRahul", "Virat"];
+  print(player2);
+
+  print(player.runtimeType);
+  print(player2.runtimeType);
+}
