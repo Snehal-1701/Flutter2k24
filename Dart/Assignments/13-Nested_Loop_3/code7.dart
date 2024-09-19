d225b8ddb5b956f4dc5bc394df88b69d1b9ab580
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-09-19 22:01:51 +0530

    Pattern of rectangular numbers

---
+++ Dart/Assignments/13-Nested_Loop_3/code7.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter no. of rows : ");
+  int row = int.parse(stdin.readLineSync()!);
+  int n = 0;
+
+  for(int i=1; i<=row; i++) {
+    for(int j=1; j<=row; j++) {
+      stdout.write("${n*(n+1)} ");
+      n++;
+    }
+    print("");
+  }
+}
