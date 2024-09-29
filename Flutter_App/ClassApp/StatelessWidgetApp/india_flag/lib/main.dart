453326b965a7232fd2eddb16ed4c8d91a11b076c
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-09-29 21:41:17 +0530

    Flag india

--- Flutter_App/ClassApp/StatelessWidgetApp/india_flag/lib/main.dart
+++ Flutter_App/ClassApp/StatelessWidgetApp/india_flag/lib/main.dart
@@ -16,42 +16,86 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 600,
-                width: 5,
-                color: Colors.black,
-              ),
-              Column(
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // const SizedBox(
+                //   width: 70,
+                // ),
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      height: 13,
+                      width: 13,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Container(
+                      height: 5,
+                      width: 5,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 500,
+                      width: 10,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    // const SizedBox(
+                    //   height: 60,
+                    // ),
+                    Container(
+                      width: 250,
+                      height: 80,
+                      color: Colors.orange,
+                    ),
+                    Container(
+                      width: 250,
+                      height: 80,
+                      color: Colors.white,
+                      child: Image.network(
+                          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Ashoka_Chakra.svg/300px-Ashoka_Chakra.svg.png"),
+                    ),
+                    Container(
+                      width: 250,
+                      height: 80,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
                 
+              ],
+            ),
+            Column(
                 children: [
-                  const SizedBox(
-                    height: 80,
+                  Container(
+                    width: 60,
+                    height: 30,
+                    color: Colors.brown,
                   ),
                   Container(
-                    width: 280,
-                    height: 100,
-                    color: Colors.orange,
+                    width: 80,
+                    height: 30,
+                    color: Colors.brown,
                   ),
                   Container(
-                    width: 280,
-                    height: 100,
-                    color: Colors.white,
-                    child: Image.network(
-                        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Ashoka_Chakra.svg/300px-Ashoka_Chakra.svg.png"),
-                  ),
-                  Container(
-                    width: 280,
-                    height: 100,
-                    color: Colors.green,
+                    width: 100,
+                    height: 30,
+                    color: Colors.brown,
                   ),
                 ],
               ),
-            ],
-          ),
+          ],
         ),
       ),
     );
