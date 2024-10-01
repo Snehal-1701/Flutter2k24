544bed83d253daf3bb07068273c8a4c88752753c
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-10-01 07:53:24 +0530

    Flag with stairs

--- Flutter_App/ClassApp/StatelessWidgetApp/india_flag/lib/main.dart
+++ Flutter_App/ClassApp/StatelessWidgetApp/india_flag/lib/main.dart
@@ -17,84 +17,101 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // const SizedBox(
-                //   width: 70,
-                // ),
-                Column(
-                  children: [
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Container(
-                      height: 13,
-                      width: 13,
+                const SizedBox(width:120),
+                Container(
+                      height: 15,
+                      width: 15,
                       decoration: const BoxDecoration(
                         shape: BoxShape.circle,
                         color: Colors.black,
                       ),
                     ),
-                    Container(
+              ],
+            ),
+            
+            Row(
+              children: [
+                const SizedBox(width:124),
+                Container(
                       height: 5,
                       width: 5,
                       color: Colors.black,
                     ),
-                    Container(
-                      height: 500,
-                      width: 10,
-                      color: Colors.black,
-                    ),
-                  ],
+              ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(width: 60,),
+
+                
+                Container(
+                  height: 450,
+                  width: 10,
+                  color: Colors.black,
                 ),
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // const SizedBox(
-                    //   height: 60,
-                    // ),
                     Container(
-                      width: 250,
-                      height: 80,
+                      width: 200,
+                      height: 50,
                       color: Colors.orange,
                     ),
                     Container(
-                      width: 250,
-                      height: 80,
+                      width: 200,
+                      height: 50,
                       color: Colors.white,
                       child: Image.network(
                           "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Ashoka_Chakra.svg/300px-Ashoka_Chakra.svg.png"),
                     ),
                     Container(
-                      width: 250,
-                      height: 80,
+                      width: 200,
+                      height: 50,
                       color: Colors.green,
                     ),
                   ],
                 ),
-                
               ],
             ),
-            Column(
-                children: [
-                  Container(
-                    width: 60,
-                    height: 30,
-                    color: Colors.brown,
-                  ),
-                  Container(
-                    width: 80,
-                    height: 30,
-                    color: Colors.brown,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 30,
-                    color: Colors.brown,
-                  ),
-                ],
-              ),
+            
+            Row(
+              children: [
+                const SizedBox(width:75),
+                Container(
+                  width: 100,
+                  height: 20,
+                  color: Colors.brown,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(width:50),
+                Container(
+                  width: 150,
+                  height: 20,
+                  color: Colors.brown,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const SizedBox(width:25),
+                Container(
+                  width: 200,
+                  height: 20,
+                  color: Colors.brown,
+                ),
+              ],
+            ),
           ],
         ),
       ),
