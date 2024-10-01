abb6aabdfd1f5fcee6a77f26aef4ccbd947357c4
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-10-01 22:49:02 +0530

    India flag with gradient background

---
+++ Flutter_App/Assignments/quizapp/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Indian Flag"),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // const SizedBox(
+                //   width: 70,
+                // ),
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      height: 13,
+                      width: 13,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Container(
+                      height: 5,
+                      width: 5,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 500,
+                      width: 10,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    // const SizedBox(
+                    //   height: 60,
+                    // ),
+                    Contai-ner(
+                      width: 250,
+                      height: 80,
+                      color: Colors.orange,
+                    ),
+                    Container(
+                      width: 250,
+                      height: 80,
+                      color: Colors.white,
+                      child: Image.network(
+                          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Ashoka_Chakra.svg/300px-Ashoka_Chakra.svg.png"),
+                    ),
+                    Container(
+                      width: 250,
+                      height: 80,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+                
+              ],
+            ),
+            Column(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 30,
+                    color: Colors.brown,
+                  ),
+                  Container(
+                    width: 80,
+                    height: 30,
+                    color: Colors.brown,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 30,
+                    color: Colors.brown,
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- Flutter_App/ClassApp/StatelessWidgetApp/india_flag/lib/main.dart
+++ Flutter_App/ClassApp/StatelessWidgetApp/india_flag/lib/main.dart
@@ -10,109 +10,121 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Indian Flag"),
           centerTitle: true,
           backgroundColor: Colors.blue,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                const SizedBox(width:120),
-                Container(
-                      height: 15,
-                      width: 15,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.orange,
+                Colors.white,
+                Colors.green,
+              ]
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  const SizedBox(width:120),
+                  Container(
+                        height: 15,
+                        width: 15,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black,
+                        ),
+                      ),
+                ],
+              ),
+              
+              Row(
+                children: [
+                  const SizedBox(width:124),
+                  Container(
+                        height: 5,
+                        width: 5,
                         color: Colors.black,
                       ),
-                    ),
-              ],
-            ),
-            
-            Row(
-              children: [
-                const SizedBox(width:124),
-                Container(
-                      height: 5,
-                      width: 5,
-                      color: Colors.black,
-                    ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(width: 60,),
-
-                
-                Container(
-                  height: 450,
-                  width: 10,
-                  color: Colors.black,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 50,
-                      color: Colors.orange,
-                    ),
-                    Container(
-                      width: 200,
-                      height: 50,
-                      color: Colors.white,
-                      child: Image.network(
-                          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Ashoka_Chakra.svg/300px-Ashoka_Chakra.svg.png"),
-                    ),
-                    Container(
-                      width: 200,
-                      height: 50,
-                      color: Colors.green,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            
-            Row(
-              children: [
-                const SizedBox(width:75),
-                Container(
-                  width: 100,
-                  height: 20,
-                  color: Colors.brown,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                const SizedBox(width:50),
-                Container(
-                  width: 150,
-                  height: 20,
-                  color: Colors.brown,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                const SizedBox(width:25),
-                Container(
-                  width: 200,
-                  height: 20,
-                  color: Colors.brown,
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(width: 60,),
+                  Container(
+                    height: 450,
+                    width: 10,
+                    color: Colors.black,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 50,
+                        color: Colors.orange,
+                      ),
+                      Container(
+                        width: 200,
+                        height: 50,
+                        color: Colors.white,
+                        child: Image.network(
+                            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Ashoka_Chakra.svg/300px-Ashoka_Chakra.svg.png"),
+                      ),
+                      Container(
+                        width: 200,
+                        height: 50,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              
+              Row(
+                children: [
+                  const SizedBox(width:75),
+                  Container(
+                    width: 100,
+                    height: 20,
+                    color: Colors.brown,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  const SizedBox(width:50),
+                  Container(
+                    width: 150,
+                    height: 20,
+                    color: Colors.brown,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  const SizedBox(width:25),
+                  Container(
+                    width: 200,
+                    height: 20,
+                    color: Colors.brown,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
