afd4e8e8755b39dd66b274d5ab105b448b690832
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-10-02 00:33:02 +0530

    Column mainAxisAlignment and crossAxisAlignment set to start and center

---
+++ Flutter_App/ClassApp/StatelessWidgetApp/column_align_parameters/lib/start_center.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+class Column2 extends StatelessWidget {
+  const Column2({super.key});
+
+  @override 
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 200,
+            width: 200,
+            color: Colors.amber,
+          ),
+        ],
+        
+      ),
+    );
+  }
+}
