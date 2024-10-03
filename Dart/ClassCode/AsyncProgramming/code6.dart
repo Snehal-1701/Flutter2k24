642c341d5ae3b5cc10bf6b64f05e6c38e54691c4
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-10-03 16:19:34 +0530

    Future

--- Dart/ClassCode/AsyncProgramming/code6.dart
+++ Dart/ClassCode/AsyncProgramming/code6.dart
@@ -1,6 +1,6 @@
 void main() {
   print("Statement 1");
-  Future<String> retVal = Future(() => "Statement 2");
+  Future<String> retVal = Future.delayed(Duration(seconds:5),() => "Statement 2");
   Future.delayed(Duration(seconds:5),() => print(retVal));
   print("Statement 4");
 }
