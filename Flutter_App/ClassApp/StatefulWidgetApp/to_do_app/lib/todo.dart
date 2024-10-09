4c900f44a085dee13b50025985c4bbb7989c7a2e
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-10-09 23:00:35 +0530

    To do UI

--- Flutter_App/ClassApp/StatefulWidgetApp/to_do_app/lib/todo.dart
+++ Flutter_App/ClassApp/StatefulWidgetApp/to_do_app/lib/todo.dart
@@ -11,7 +11,12 @@ class To_Do_UI extends StatefulWidget {
 
 class _To_Do_UI extends State<To_Do_UI> {
 
-  List colorList = [];
+  List colorList = [
+    const Color.fromRGBO(250, 232, 232, 1),
+    const Color.fromRGBO(232, 237, 250, 1),
+    const Color.fromRGBO(250, 249, 232, 1),
+    const Color.fromRGBO(250, 232, 250, 1),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,131 +25,125 @@ class _To_Do_UI extends State<To_Do_UI> {
         title: Text(
           "To-do list",
           style: GoogleFonts.quicksand(
-            fontSize: 26,
+            fontSize: 30,
             fontWeight: FontWeight.w700,
             color: Colors.white,
           ),
         ),
       ),
-      body: Card(),
+      body: buildCard(),
     );
   }
 
-  Widget Card() {
+  Widget buildCard() {
     return ListView.builder(
-        itemCount: 6,
+        itemCount: 8,
         itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Container(
-              // height: 112,
-              width: 330,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color.fromRGBO(250, 232, 232, 1),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        children: [
-                          const SizedBox(
-                            height:18
+          return Container(
+            margin: const EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: colorList[index % colorList.length],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        const SizedBox(height: 18),
+                        Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          height: 80,
+                          width: 80,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
                           ),
-                          Container(
-                            margin: const EdgeInsets.only(left: 10),
-                            height: 80,
-                            width: 80,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                            ),
-                            child: const Icon(
-                              Icons.image_outlined,
-                              color: Colors.grey,
-                              size: 50,
-                            ),
+                          child: const Icon(
+                            Icons.image_outlined,
+                            color: Colors.grey,
+                            size: 50,
                           ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0, left: 10),
-                            child: SizedBox(
-                              width: 255,
-                              child: Text(
-                                "Lorem Ipsum is simply setting industry.",
-                                style: GoogleFonts.quicksand(
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w700,
-                                ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0, left: 10),
+                          child: SizedBox(
+                            width: 255,
+                            child: Text(
+                              "Lorem Ipsum is simply setting industry.",
+                              style: GoogleFonts.quicksand(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w700,
                               ),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0, left: 10),
-                            child: SizedBox(
-                              width: 257,
-                              child: Text(
-                                "Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-                                style: GoogleFonts.quicksand(
-                                  fontSize: 11,
-                                  fontWeight: FontWeight.w500,
-                                  color: const Color.fromRGBO(84, 84, 84, 1),
-                                ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0, left: 10),
+                          child: SizedBox(
+                            width: 257,
+                            child: Text(
+                              "Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                              style: GoogleFonts.quicksand(
+                                fontSize: 11,
+                                fontWeight: FontWeight.w500,
+                                color: const Color.fromRGBO(84, 84, 84, 1),
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          "10 July 2023",
-                          style: GoogleFonts.quicksand(
-                            fontSize: 11,
-                            fontWeight: FontWeight.w500,
-                            color: Color.fromRGBO(84, 84, 84, 1),
-                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        "10 July 2023",
+                        style: GoogleFonts.quicksand(
+                          fontSize: 11,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromRGBO(84, 84, 84, 1),
                         ),
                       ),
-                      const Spacer(),
-                      SizedBox(
-                        width: 15,
-                        height: 15,
-                        child: Image.asset(
-                          "assets/svg/edit_logo.png",
-                        ),
+                    ),
+                    const Spacer(),
+                    SizedBox(
+                      width: 17,
+                      height: 17,
+                      child: Image.asset(
+                        "assets/svg/edit_logo.png",
                       ),
-                      const SizedBox(
-                        width: 5,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    SizedBox(
+                      width: 17,
+                      height: 17,
+                      child: Image.asset(
+                        "assets/svg/delete_icon.png",
                       ),
-                      SizedBox(
-                        width: 17,
-                        height: 17,
-                        child: Image.asset(
-                          "assets/svg/delete_icon.png",
-                        ),
-                        // Icon(
-                        //   Icons.delete,
-                        //   color: Color.fromRGBO(0, 139, 148, 1),
-                        // ),
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                      // Icon(
+                      //   Icons.delete,
+                      //   color: Color.fromRGBO(0, 139, 148, 1),
+                      // ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                  ],
+                ),
+              ],
             ),
           );
         });
