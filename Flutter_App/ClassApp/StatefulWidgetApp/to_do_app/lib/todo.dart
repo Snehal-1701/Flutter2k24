28433a1e59d96fa769e4589885c10894448432cf
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-10-21 23:08:53 +0530

    Todo Functionality

--- Flutter_App/ClassApp/StatefulWidgetApp/to_do_app/lib/todo.dart
+++ Flutter_App/ClassApp/StatefulWidgetApp/to_do_app/lib/todo.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'todo_model.dart';
+import 'package:intl/intl.dart';
 // import 'card.dart';
 
 class TodoAppUI extends StatefulWidget {
@@ -10,6 +13,25 @@ class TodoAppUI extends StatefulWidget {
 }
 
 class _TodoAppUIState extends State<TodoAppUI> {
+  ///TITLE TEXTEDITING CONTROLLER
+  TextEditingController titleController = TextEditingController();
+
+  ///DESCRIPTION TEXTEDITING CONTROLLER
+  TextEditingController descriptionController = TextEditingController();
+
+  ///DATE TEXTEDITING CONTROLLER
+  TextEditingController dateController = TextEditingController();
+
+  ///TITLE EMPTY
+  bool? titleEmpty;
+
+  ///DESCRIPTION EMPTY
+  bool? descriptionEmpty;
+
+  ///DATE EMPTY
+  bool? dateEmpty;
+
+  ///COLOR LIST
   List colorList = [
     const Color.fromRGBO(250, 232, 232, 1),
     const Color.fromRGBO(232, 237, 250, 1),
@@ -17,11 +39,21 @@ class _TodoAppUIState extends State<TodoAppUI> {
     const Color.fromRGBO(250, 232, 250, 1),
   ];
 
+  ///PROFILE IMAGE LIST
   List<String> imageList = [
     "assets/profile1.jpg",
     "assets/profile2.png",
     "assets/profile3.jpg",
     "assets/profile4.png",
+  ];
+
+  ///LIST OF TODOMODEL
+  List<TodoModel> listOfTask = [
+    TodoModel(
+      title: "Flutter",
+      description: "Building to do UI",
+      date: "10 june 2024",
+    ),
   ];
 
   @override
@@ -48,7 +80,10 @@ class _TodoAppUIState extends State<TodoAppUI> {
         ),
         child: GestureDetector(
           onTap: () {
-            bottomSheet();
+            titleEmpty = false;
+            descriptionEmpty = false;
+            dateEmpty = false;
+            showBottomSheet(false);
           },
           child: const Icon(
             Icons.add,
@@ -60,9 +95,68 @@ class _TodoAppUIState extends State<TodoAppUI> {
     );
   }
 
+  ///SUBMIT
+  void submitData(bool isEdit, [TodoModel? obj]) {
+    // titleEmpty = titleController.text.trim().isEmpty;
+    // descriptionEmpty = descriptionController.text.trim().isEmpty;
+    // dateEmpty = dateController.text.trim().isEmpty;
+
+    if (titleEmpty == false &&
+        descriptionEmpty == false &&
+        dateEmpty == false) {
+      if (isEdit) {
+        obj!.title = titleController.text;
+        obj.description = descriptionController.text;
+        obj.date = dateController.text;
+      } else {
+        listOfTask.add(
+          TodoModel(
+            title: titleController.text,
+            date: dateController.text,
+            description: descriptionController.text,
+          ),
+        );
+      }
+      clearController();
+      Navigator.of(context).pop();
+    }
+    setState(() {});
+  }
+
+  // void submitData(bool isEdit, [TodoModel? obj]) {
+  //   if (titleController.text.trim().isNotEmpty &&
+  //       descriptionController.text.trim().isNotEmpty &&
+  //       dateController.text.trim().isNotEmpty) {
+  //     if (isEdit) {
+  //       obj!.title = titleController.text;
+  //       obj.description = descriptionController.text;
+  //       obj.date = dateController.text;
+  //     } else {
+  //       listOfTask.add(
+  //         TodoModel(
+  //           title: titleController.text,
+  //           date: dateController.text,
+  //           description: descriptionController.text,
+  //         ),
+  //       );
+  //     }
+  //     clearController();
+  //     Navigator.of(context).pop();
+  //     setState(() {});
+  //   }
+  // }
+
+  ///CLEAR CONTROLLER
+  void clearController() {
+    titleController.clear();
+    descriptionController.clear();
+    dateController.clear();
+  }
+
+  ///TODO CARD
   Widget buildCard() {
     return ListView.builder(
-        itemCount: 8,
+        itemCount: listOfTask.length,
         itemBuilder: (BuildContext context, int index) {
           return Container(
             margin: const EdgeInsets.all(15.0),
@@ -70,7 +164,6 @@ class _TodoAppUIState extends State<TodoAppUI> {
               borderRadius: BorderRadius.circular(15),
               color: colorList[index % colorList.length],
             ),
-            
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -79,8 +172,11 @@ class _TodoAppUIState extends State<TodoAppUI> {
                     Column(
                       children: [
                         const SizedBox(height: 18),
+
+                        ///PROFILE IMAGE
                         Container(
                           margin: const EdgeInsets.only(left: 10),
+                          padding: const EdgeInsets.all(20),
                           height: 80,
                           width: 80,
                           decoration: const BoxDecoration(
@@ -88,12 +184,8 @@ class _TodoAppUIState extends State<TodoAppUI> {
                             color: Colors.white,
                           ),
                           clipBehavior: Clip.antiAlias,
-                          child: Image.asset(imageList[index%imageList.length]),
-                          // child: const Icon(
-                          //   Icons.image_outlined,
-                          //   color: Colors.grey,
-                          //   size: 50,
-                          // ),
+                          child:
+                              SvgPicture.asset("assets/svg/profileImage.svg"),
                         ),
                       ],
                     ),
@@ -104,8 +196,10 @@ class _TodoAppUIState extends State<TodoAppUI> {
                           padding: const EdgeInsets.only(top: 8.0, left: 10),
                           child: SizedBox(
                             width: 255,
+
+                            ///TITLE
                             child: Text(
-                              "Lorem Ipsum is simply setting industry.",
+                              listOfTask[index].title,
                               style: GoogleFonts.quicksand(
                                 fontSize: 13,
                                 fontWeight: FontWeight.w700,
@@ -117,8 +211,10 @@ class _TodoAppUIState extends State<TodoAppUI> {
                           padding: const EdgeInsets.only(top: 8.0, left: 10),
                           child: SizedBox(
                             width: 257,
+
+                            ///DESCRIPTION
                             child: Text(
-                              "Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                              listOfTask[index].description,
                               style: GoogleFonts.quicksand(
                                 fontSize: 11,
                                 fontWeight: FontWeight.w500,
@@ -135,8 +231,10 @@ class _TodoAppUIState extends State<TodoAppUI> {
                   children: [
                     Padding(
                       padding: const EdgeInsets.all(10.0),
+
+                      ///DATE
                       child: Text(
-                        "10 July 2023",
+                        listOfTask[index].date,
                         style: GoogleFonts.quicksand(
                           fontSize: 11,
                           fontWeight: FontWeight.w500,
@@ -145,26 +243,36 @@ class _TodoAppUIState extends State<TodoAppUI> {
                       ),
                     ),
                     const Spacer(),
-                    SizedBox(
-                      width: 17,
-                      height: 17,
-                      child: Image.asset(
-                        "assets/svg/edit_logo.png",
+
+                    ///EDIT ICON
+                    GestureDetector(
+                      onTap: () {
+                        titleEmpty = false;
+                        descriptionEmpty = false;
+                        dateEmpty = false;
+                        titleController.text = listOfTask[index].title;
+                        descriptionController.text =
+                            listOfTask[index].description;
+                        dateController.text = listOfTask[index].date;
+                        showBottomSheet(true, listOfTask[index]);
+                      },
+                      child: const Icon(
+                        Icons.edit_outlined,
+                        color: Color.fromRGBO(2, 167, 177, 1),
                       ),
                     ),
                     const SizedBox(
                       width: 5,
                     ),
-                    SizedBox(
-                      width: 17,
-                      height: 17,
-                      child: Image.asset(
-                        "assets/svg/delete_icon.png",
+                    GestureDetector(
+                      onTap: () {
+                        listOfTask.removeAt(index);
+                        setState(() {});
+                      },
+                      child: const Icon(
+                        Icons.delete_outlined,
+                        color: Color.fromRGBO(2, 167, 177, 1),
                       ),
-                      // Icon(
-                      //   Icons.delete,
-                      //   color: Color.fromRGBO(0, 139, 148, 1),
-                      // ),
                     ),
                     const SizedBox(
                       width: 5,
@@ -177,7 +285,8 @@ class _TodoAppUIState extends State<TodoAppUI> {
         });
   }
 
-  void bottomSheet() {
+  ///BOTTOMSHEET
+  void showBottomSheet(bool isEdit, [TodoModel? obj]) {
     showModalBottomSheet(
         context: context,
         isScrollControlled: true,
@@ -194,7 +303,7 @@ class _TodoAppUIState extends State<TodoAppUI> {
               children: [
                 Center(
                   child: Text(
-                    "Create To-Do ",
+                    "Create Task ",
                     style: GoogleFonts.quicksand(
                       fontSize: 26,
                       fontWeight: FontWeight.w700,
@@ -205,22 +314,38 @@ class _TodoAppUIState extends State<TodoAppUI> {
                 const SizedBox(
                   height: 15,
                 ),
-                Text(
-                  "Title",
-                  style: GoogleFonts.quicksand(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                    color: const Color.fromRGBO(0, 139, 148, 1),
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "Lorem Ipsum typeseting industry. ",
-                    hintStyle: GoogleFonts.quicksand(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                      color: const Color.fromRGBO(0, 0, 0, 1),
+                Row(
+                  children: [
+                    Text(
+                      "Title",
+                      style: GoogleFonts.quicksand(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                        color: const Color.fromRGBO(0, 139, 148, 1),
+                      ),
                     ),
+                    const SizedBox(width: 10),
+                    Text(
+                      titleEmpty == true ? "Please add title" : " ",
+                      style: GoogleFonts.quicksand(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+
+                ///TEXTFIELD - TITLE
+                TextField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    // hintText: "Enter title here",
+                    // hintStyle: GoogleFonts.quicksand(
+                    //   fontSize: 15,
+                    //   fontWeight: FontWeight.w400,
+                    //   color: const Color.fromRGBO(0, 0, 0, 1),
+                    // ),
                     enabledBorder: const OutlineInputBorder(
                       borderSide: BorderSide(
                         width: 1,
@@ -245,25 +370,39 @@ class _TodoAppUIState extends State<TodoAppUI> {
                 const SizedBox(
                   height: 15,
                 ),
-                Text(
-                  "Description",
-                  style: GoogleFonts.quicksand(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                    color: const Color.fromRGBO(0, 139, 148, 1),
-                  ),
+                Row(
+                  children: [
+                    Text(
+                      "Description",
+                      style: GoogleFonts.quicksand(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                        color: const Color.fromRGBO(0, 139, 148, 1),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      descriptionEmpty == true ? "Please add description" : " ",
+                      style: GoogleFonts.quicksand(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
                 ),
+
+                ///TEXTFIELD - DESCRIPTION
                 TextField(
+                  controller: descriptionController,
                   maxLines: 3,
-                  decoration: InputDecoration(
-                 
-                    hintText:
-                        "TSimply dummy text of the printing and  has been the typesetting Lorem Ipsum has been the industry.",
-                    hintStyle: GoogleFonts.quicksand(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                      color: const Color.fromRGBO(0, 0, 0, 1),
-                    ),
+                  decoration: const InputDecoration(
+                    // hintText: "Enter description here",
+                    // hintStyle: GoogleFonts.quicksand(
+                    //   fontSize: 15,
+                    //   fontWeight: FontWeight.w400,
+                    //   color: const Color.fromRGBO(0, 0, 0, 1),
+                    // ),
                     enabledBorder: const OutlineInputBorder(
                       borderSide: BorderSide(
                         width: 1,
@@ -288,21 +427,51 @@ class _TodoAppUIState extends State<TodoAppUI> {
                 const SizedBox(
                   height: 15,
                 ),
-                Text(
-                  "Date",
-                  style: GoogleFonts.quicksand(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 14,
-                    color: const Color.fromRGBO(0, 139, 148, 1),
-                  ),
+                Row(
+                  children: [
+                    Text(
+                      "Date",
+                      style: GoogleFonts.quicksand(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        color: const Color.fromRGBO(0, 139, 148, 1),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      dateEmpty == true ? "Please add date" : " ",
+                      style: GoogleFonts.quicksand(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
                 ),
+
+                ///DATEPICKER
                 TextField(
-                  
+                  controller: dateController,
+                  readOnly: true,
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime(2024),
+                      lastDate: DateTime(2025),
+                    );
+
+                    String formattedDate =
+                        DateFormat.yMMMd().format(pickedDate!);
+
+                    setState(() {
+                      dateController.text = formattedDate;
+                    });
+                  },
                   decoration: InputDecoration(
                     suffixIcon: const Icon(
                       Icons.calendar_month_outlined,
                     ),
-                    hintText: "2023-07-26",
+                    // hintText: "add date",
                     hintStyle: GoogleFonts.quicksand(
                       fontSize: 15,
                       fontWeight: FontWeight.w400,
@@ -329,27 +498,45 @@ class _TodoAppUIState extends State<TodoAppUI> {
                     ),
                   ),
                 ),
-                const SizedBox(height:20),
+                const SizedBox(height: 20),
+
+                ///SUBMIT
                 Center(
-                  child: Container(
-                    width: 320,
-                    height: 50,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: const Color.fromRGBO(0,139,148,1),
-                    ),
-                    child: Text(
-                      "Submit",
-                      style: GoogleFonts.quicksand(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
+                  child: GestureDetector(
+                    onTap: () {
+                      titleEmpty = titleController.text.trim().isEmpty;
+                      descriptionEmpty =
+                          descriptionController.text.trim().isEmpty;
+                      dateEmpty = dateController.text.trim().isEmpty;
+                      setState(() {
+                        // isEdit ? submitData(isEdit, obj) : submitData(isEdit);
+                        if (isEdit) {
+                          submitData(isEdit, obj!);
+                        } else {
+                          submitData(isEdit);
+                        }
+                      });
+                    },
+                    child: Container(
+                      width: 320,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: const Color.fromRGBO(0, 139, 148, 1),
+                      ),
+                      child: Text(
+                        "Submit",
+                        style: GoogleFonts.quicksand(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ),
-                const SizedBox(height:20),
+                const SizedBox(height: 20),
               ],
             ),
           );
--- Flutter_App/ClassApp/StatefulWidgetApp/to_do_app/lib/todo_model.dart
+++ Flutter_App/ClassApp/StatefulWidgetApp/to_do_app/lib/todo_model.dart
@@ -1,7 +1,7 @@
 class TodoModel {
-  String? title;
-  String? description;
-  String? date;
+  String title;
+  String description;
+  String date;
 
-  TodoModel({required title, required description, required date});
+  TodoModel({required this.title, required this.description, required this.date});
 }
